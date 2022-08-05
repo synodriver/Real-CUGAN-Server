6296866abe1cb8bd175bc51beb0c9bc7d1acc435
synodriver <624805065@qq.com>
2022-08-05 21:04:06 +0800

    update docs (#2)
    * use fastapi
    
    * add requirements
    
    * fix fastapi
    
    * add description
    
    * pytorch have released gil, so we just put it into a threadpool to use multicore
    
    * Update README.md
    
    * Update server.py
    
    * update readme
    
    * add response format support
    
    * add clear cache api
    
    * update dep
    
    Co-authored-by: 源文雨 <41315874+fumiama@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,20 +1,21 @@
+import os
+import time
 from hashlib import md5
 from io import BytesIO
-from os.path import exists, abspath, dirname, join
+from os.path import abspath, dirname, exists, join
 from typing import Optional
 
-from cv2 import IMREAD_UNCHANGED, imdecode, imencode
-
-from fastapi import FastAPI, Query, File, UploadFile
-from fastapi.responses import UJSONResponse, StreamingResponse
+import aiofiles
 import aiohttp
-
+from cv2 import IMREAD_UNCHANGED, imdecode, imencode
+from fastapi import FastAPI, File, Query, Request, UploadFile
+from fastapi.responses import StreamingResponse, UJSONResponse
 from numpy import frombuffer, uint8
 
 from .upcunet_v3 import RealWaifuUpScaler
 from .utils import run_sync
 
-app = FastAPI(title=__name__)
+app = FastAPI(title="图片放大器")
 
 ups = {}
 
@@ -55,6 +56,7 @@ async def scale(
     scale: Optional[int] = Query(2, description="scale size 2-4"),
     tile: Optional[int] = Query(2, description="tile 0-9"),
     url: Optional[str] = Query(None, description="url of a picture"),
+    format: Optional[str] = Query("png", description="response format"),
 ):
     if model not in MODEL_LIST:
         return UJSONResponse({"status": "no such model"}, 400)
@@ -78,18 +80,18 @@ async def scale(
         CURRENT_FOLDER, "tmp", md5(data + f"{model}_{tile}".encode()).hexdigest()
     )  # todo 加个选项控制缓存路径
     if exists(path):
-        with open(path, "rb") as f:
-            data = f.read()
+        async with aiofiles.open(path, "rb") as f:
+            data = await f.read()
     else:
         _, data = imencode(
-            ".webp", await calcdata(model, scale, tile, data)
+            f".{format}", await calcdata(model, scale, tile, data)
         )  # todo 这里改成传memoryview?
         data = data.tobytes()
         if not data:
             return UJSONResponse({"status": "zero output data len"}, 500)
-        with open(path, "wb") as f:
-            f.write(data)
-    return StreamingResponse(BytesIO(data), 200, {"Content-Type": "image/webp"})
+        async with aiofiles.open(path, "wb") as f:
+            await f.write(data)
+    return StreamingResponse(BytesIO(data), 200, {"Content-Type": f"image/{format}"})
 
 
 @app.post("/scale")
@@ -98,6 +100,7 @@ async def scale_(
     scale: Optional[int] = Query(2, description="scale size 2-4"),
     tile: Optional[int] = Query(2, description="tile 0-9"),
     file: UploadFile = File(..., description="a picture"),
+    format: Optional[str] = Query("png", description="response format"),
 ):
     if model not in MODEL_LIST:
         return UJSONResponse({"status": "no such model"}, 400)
@@ -117,14 +120,32 @@ async def scale_(
         CURRENT_FOLDER, "tmp", md5(data + f"{model}_{tile}".encode()).hexdigest()
     )  # todo 加个选项控制缓存路径
     if exists(path):
-        with open(path, "rb") as f:
-            data = f.read()
+        async with aiofiles.open(path, "rb") as f:
+            data = await f.read()
     else:
-        _, data = imencode(".webp", await calcdata(model, scale, tile, data))
+        _, data = imencode(f".{format}", await calcdata(model, scale, tile, data))
         data = data.tobytes()
         if not data:
             return UJSONResponse({"status": "zero output data len"}, 500)
-        with open(path, "wb") as f:
-            f.write(data)
+        async with aiofiles.open(path, "wb") as f:
+            await f.write(data)
     # print(len(data))
-    return StreamingResponse(BytesIO(data), 200, {"Content-Type": "image/webp"})
+    return StreamingResponse(BytesIO(data), 200, {"Content-Type": f"image/{format}"})
+
+
+@app.get("/clear")
+async def handle_clear():
+    path = join(CURRENT_FOLDER, "tmp")  # todo 加个选项控制缓存路径
+    for p in os.listdir(path):
+        if os.path.split(p)[-1] != "README.md":
+            os.remove(join(path, p))
+    return UJSONResponse({"status": "0"}, 200)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
